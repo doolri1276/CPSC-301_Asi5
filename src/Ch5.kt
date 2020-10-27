568791493badeaf4941d52392aeec23f6d72e5a7
KyungminUm <snownaul2@gmail.com>
2020-10-26 23:33:37 -0400

    -

---
+++ src/Ch5.kt
@@ -0,0 +1,69 @@
+
+fun main(){
+
+}
+
+//2 JK
+fun ordlist(){
+
+}
+
+//4 JK
+fun multpairs(){
+
+}
+
+//6 KM ************************
+fun sqsum(){
+
+}
+
+//8 JK
+fun band(){
+
+}
+
+//10 KM ************************
+fun dupList(){
+
+}
+
+//12 JK
+fun il2absrl(){
+
+}
+
+//14 KM ************************
+fun maxpairs(){
+
+}
+
+//16 JK
+fun lconcat(){
+
+}
+
+//18 KM ************************
+fun min(){
+
+}
+
+//20 SJ
+fun append(){
+
+}
+
+//22 SJ
+fun evens(){
+
+}
+
+//24 SJ
+fun mymap(){
+
+}
+
+//26 SJ
+fun mymap2(){
+
+}
